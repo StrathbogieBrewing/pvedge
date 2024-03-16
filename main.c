83793fd419127aa1cba97edb86f7b85fdae4f002
Johny Hoops <johnyhoops@gmail.com>
2024-03-16 15:25:43 +1100

    revert to using original fet drivers

--- main.c
+++ main.c
@@ -24,10 +24,21 @@
 
 #define SOLAR_ADC_BIT_MV (346)
 
-//#define SOLAR_START (65000 / SOLAR_ADC_BIT_MV)
-#define SOLAR_START (28000 / SOLAR_ADC_BIT_MV)
+#define SOLAR_START (65000 / SOLAR_ADC_BIT_MV)
+//#define SOLAR_START (28000 / SOLAR_ADC_BIT_MV)
 #define SOLAR_STOP (24000 / SOLAR_ADC_BIT_MV)
-#define FAN_START (48000 / SOLAR_ADC_BIT_MV)
+#define FAN_START_TEMP (179)
+
+#define THERMAL_STOP_TEMP (189)
+
+#define SOLAR_VOLTAGE_ADC_CHANNEL 0
+#define HEATSINK_TEMP_ADC_CHANNEL 2
+
+#define LEFT_LOW RC3
+#define LEFT_HIGH RC2
+#define RIGHT_LOW RC0
+#define RIGHT_HIGH RC1
+
 
 void tick_delay(unsigned char ticks){
     while(ticks--){
@@ -52,30 +63,37 @@ void main(void)
     ADCON0 = 0x81;               // Turn ON ADC and Clock Selection
     ADCON1 = 0x00;               // All pins as Analog Input and setting Reference Voltages
     ADCON0 &= 0xC5;              // Select channel 0 - solar voltage
+    ADCON0 |= (SOLAR_VOLTAGE_ADC_CHANNEL << 3);
+    
     unsigned char on_ticks = 0;  // start with power at zero
     unsigned char cycle_counter = 0;  // start with power at zero
     
+    unsigned char solar_voltage = 0;
+    unsigned char heatsink_temp = 0;
+    
     while(1)
     {
-        asm("CLRWDT");
+        CLRWDT();
         
-        GO_nDONE = 1;                //Initializes A/D conversion
-        while(GO_nDONE);             //Waiting for conversion to complete
-        unsigned char solar_voltage = ADRES;
+        // Read adc solar voltage
+        GO_nDONE = 1;                // Initializes A/D conversion
+        while(GO_nDONE);             // Waiting for conversion to complete
+        solar_voltage = ADRES;
+        ADCON0 &= 0xC5;              // Select channel 2 - heatsink temperature
+        ADCON0 |= (HEATSINK_TEMP_ADC_CHANNEL << 3);         
         
-        if(solar_voltage < SOLAR_STOP){
-            on_ticks = 0;
-        }
-        
-        if((solar_voltage > FAN_START) && (on_ticks > (TICKS_ON_MAX * 1) / 4)){
+        // update fan state
+        if((solar_voltage > SOLAR_STOP) 
+                && (on_ticks > (TICKS_ON_MAX / 10)) 
+                && (heatsink_temp > FAN_START_TEMP)){
             RC5 = 1;    // fan on
         } else {
             RC5 = 0;    // fan off
         }
         
+        // update duty cycle every 16 ac line cycles
         cycle_counter++;
         if((cycle_counter & 0x0f) == 0){
-            // update duty cycle every 16 cycles
             if(solar_voltage < SOLAR_START){
                 if(on_ticks){
                     on_ticks--;
@@ -87,6 +105,37 @@ void main(void)
             }
         }
         
+        // shutdown tests
+        if((solar_voltage < SOLAR_STOP) || (heatsink_temp > THERMAL_STOP_TEMP)){
+            on_ticks = 0;
+        }
+        
+        // duty cycle range sanity check
+        if(on_ticks > TICKS_ON_MAX) on_ticks = TICKS_ON_MAX;
+        unsigned char off_ticks = TICKS_HALF_CYCLE - on_ticks;
+        
+        // positive half cycle
+        if(on_ticks){ 
+            LEFT_LOW = 0;    // disable left arm low side fet
+            NOP();             // shoot through guard
+            NOP();
+            LEFT_HIGH = 1;    // enable left arm high side fet
+            tick_delay(on_ticks);
+            RIGHT_LOW = 0;    // disable right arm low side fet
+            NOP();
+            NOP();
+            RIGHT_HIGH = 1;    // enable right arm high side fet  
+        } 
+        tick_delay(off_ticks);
+        
+        // Read adc heatsink temperature
+        GO_nDONE = 1;                // Initializes A/D conversion
+        while(GO_nDONE);             // Waiting for conversion to complete
+        heatsink_temp = ADRES;
+        ADCON0 &= 0xC5;              // // Select solar voltage channel 0 
+        ADCON0 |= (SOLAR_VOLTAGE_ADC_CHANNEL << 3); 
+        
+        // update leds
         PORTB |= 0x0f;
         if(on_ticks == TICKS_ON_MAX){
             PORTB &= 0xf0;
@@ -97,25 +146,20 @@ void main(void)
         } else if (on_ticks > (TICKS_ON_MAX * 1) / 4){
             PORTB &= 0xf7;
         }
-        
-        if(on_ticks > TICKS_ON_MAX) on_ticks = TICKS_ON_MAX;
-        unsigned char off_ticks = TICKS_HALF_CYCLE - on_ticks;
-        
-        if(on_ticks){
-            RC3 = 1;    // disable left arm low side fet
-            RC2 = 1;    // enable left arm high side fet
-            tick_delay(on_ticks);
-            RC0 = 1;    // disable right arm low side fet
-            RC1 = 1;    // enable right arm high side fet          
-            tick_delay(off_ticks);
             
-            RC2 = 0;    // disable left arm high side fet
-            RC3 = 0;    // enable left arm low side fet
+        // negative half cycle
+        if(on_ticks){ 
+            LEFT_HIGH = 0;    // disable left arm high side fet
+            NOP();             // shoot through guard
+            NOP();
+            LEFT_LOW = 1;    // enable left arm low side fet
             tick_delay(on_ticks);
-            RC1 = 0;    // disable right arm high side fet
-            RC0 = 0;    // enable right arm low side fet
-            tick_delay(off_ticks);
+            RIGHT_HIGH = 0;    // disable right arm high side fet
+            NOP();             // shoot through guard
+            NOP();
+            RIGHT_LOW = 1;    // enable right arm low side fet  
         }
+        tick_delay(off_ticks);
     }
 }
 
